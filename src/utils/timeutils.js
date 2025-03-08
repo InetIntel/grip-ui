b94cc4ca9741ef159539c276e2a1a59b5ea2a51f
VijayrajS <vijayrajshanmugarajt@gmail.com>
2025-03-07 22:30:28 -0500

    Issue 26: added dev note to describe intent of timeutils functions

--- src/utils/timeutils.js
+++ src/utils/timeutils.js
@@ -1,5 +1,17 @@
 import moment from "moment";
 
+/*
+* VijayrajS: The GRIP-UI website currently displays all times in UTC. The following helper functions
+* help converting local time to UTC, however, the timezone in all moment and date objects is still the
+* local timezone of the user. 
+* 
+* This was the easiest method to store all timestamps in the search objects in UTC, rather than converting
+* it every time before extracting and modifying timestamp (moment and Date) objects.
+*
+* The offsetTimezoneToUTC function obtains the timezone offset from UTC and adds it to the inputted time object.
+* Ex. offsetTimezoneToUTC(10:00 AM EST) = 3AM EST (Since EST is UTC-5)
+*/
+
 function offsetTimezoneToUTC(date) {
   const offset = date.getTimezoneOffset() * 60 * 1000;
   return new Date(date.getTime() + offset);
