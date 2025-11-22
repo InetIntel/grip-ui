60cb3e0937129bbb4916e92e10032eaec0afd323
VijayrajS <vijayrajshanmugarajt@gmail.com>
2025-11-21 20:14:10 -0800

    Issue 45: Asn.jsx cleanup : export

--- src/components/utils/AsnUtils.js
+++ src/components/utils/AsnUtils.js
@@ -2,12 +2,12 @@ export function countryCodeToFlagEmoji(countryCode) {
 	// Convert country code to corresponding emoji flag
 	//
 	// Input: 
-	//   country_code: string, 2-letter country code
+	//   countryCode: string, 2-letter country code
 	// Output:
 	//   string: emoji flag or empty string if invalid input
 
 	const EMOJI_OFFSET = 127397;
-	const countryCodeUpperCase = country_code.toUpperCase();
+	const countryCodeUpperCase = countryCode.toUpperCase();
 
 	if (typeof countryCode !== 'string' 
 			&& !/^[A-Z]{2}$/.test(countryCodeUpperCase)) {
