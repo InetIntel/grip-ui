163a468dfbd8a9e824e6b78fbf0594a11e8ed1ad
VijayrajS <vijayrajshanmugarajt@gmail.com>
2025-12-20 20:02:25 -0800

    Issue 45: Asn.jsx ? flag add

--- src/components/AsnLabel.jsx
+++ src/components/AsnLabel.jsx
@@ -90,7 +90,7 @@ function AsnLabel({ asn, data }) {
 			<span className="asn-label">
 				<span className="asn__country">
 					{countryFlag || (
-						<img src={UnknownFlag} alt="Unknown flag" className="asn__flag" />
+						<img src={UnknownFlag} alt="<?>" className="asn__flag" />
 					)}
 				</span>
 				<a href={ASRANK_URL + asNumber} target="_blank" rel="noopener noreferrer">
